c43ca6b1e8a8bce48f888dfd65155dd2350a8143
pradeep <pradeepmaurya.cse@gmail.com>
2022-08-14 23:28:35 +0530

    PL SQL

--- sql_procedure.sql
+++ sql_procedure.sql
@@ -52,5 +52,54 @@ DELIMITER ;
 call getEmpByID(1);
 call getEmpByID(7);
 
+delimiter //
+create procedure P1()
+begin
+declare x int;
+declare y int;
+declare z int;
+set x=80;
+set y=20;
+set z=x+y;
+select z;
+end//
+
+call p1();
+
+DROP procedure IF EXISTS `P2`;
+delimiter //
+create procedure P2(in x int,in y int)
+begin
+declare z int;
+set z=x+y;
+select z as "addition";
+end//
+-- call the procedure
+call p2(10,30);
+
+-- example out type of parameter in procedure
+delimiter //
+create procedure P3(in x int,in y int,out z int)
+begin
+set z=x+y;
+end//
+
+-- call the procedure
+call p3(10,20,@d);
+select @d;
 
 
+delimiter //
+create procedure emp_limit(in startin int,in ending int)
+begin
+select * from emp limit startin,ending;
+end//
+
+call emp_limit(4, 5);
+
+select count(*)  from emp group by occ 
+order by count(*) desc limit 3
+; 
+
+select * from emp;
+update emp set occ='it' where id=3;
